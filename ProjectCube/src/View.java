6513a9a15459f198ffb75c5e43d80b80466c829b
nlarsen <n.larsen@tcu.edu>
2018-03-06 21:18:38 -0600

    Add updateView in the vew.

--- ProjectCube/src/View.java
+++ ProjectCube/src/View.java
@@ -14,6 +14,27 @@ public class View extends JFrame{
 
 	//private JFrame frame;
 
+	protected final int TOP_FRONT = 0;
+	protected final int TOP_RIGHT = 1;
+	protected final int TOP_BACK = 2;
+	protected final int TOP_LEFT = 3;
+	protected final int BOTTOM_FRONT = 4;
+	protected final int BOTTOM_RIGHT = 5;
+	protected final int BOTTOM_BACK = 6;
+	protected final int BOTTOM_LEFT = 7;
+	protected final int FRONT_RIGHT = 8;
+	protected final int FRONT_LEFT = 9;
+	protected final int BACK_RIGHT = 10;
+	protected final int BACK_LEFT = 11;
+	
+	protected final int TOP_FRONT_LEFT = 0;
+	protected final int TOP_FRONT_RIGHT = 1;
+	protected final int TOP_BACK_RIGHT = 2;
+	protected final int TOP_BACK_LEFT = 3;
+	protected final int BOTTOM_FRONT_LEFT = 4;
+	protected final int BOTTOM_FRONT_RIGHT = 5;
+	protected final int BOTTOM_BACK_LEFT = 6;
+	protected final int BOTTOM_BACK_RIGHT = 7;
 
 
 	/**
@@ -63,16 +84,91 @@ public class View extends JFrame{
 		
 		
 	}
+	
 	private void initButtons(JButton[] buttons, Color c, int x, int y, int w, JFrame f) {
 		int d = w + 2;
 		for (int i = 0; i < up.length; i++) {
-			buttons[i] = new JButton(i+"");
+			buttons[i] = new JButton("");
 			buttons[i].setBackground(c);
 			buttons[i].setForeground(c);
 			buttons[i].setBorderPainted(false);
 			buttons[i].setOpaque(true);
 			f.getContentPane().add(buttons[i]);
 			buttons[i].setBounds(x + d*(i%3)	,y + d*(i/3)	,w, w);
+		}
+	}
+	public void updateView(Cube cube) {
+		//sets top layer wedges 
+		up[7].setBackground(getColor(cube.w[0].primaryColor));
+		front[1].setBackground(getColor(cube.w[0].secondaryColor));
+		up[5].setBackground(getColor(cube.w[1].primaryColor));
+		right[1].setBackground(getColor(cube.w[1].secondaryColor));
+		up[1].setBackground(getColor(cube.w[2].primaryColor));
+		back[7].setBackground(getColor(cube.w[2].secondaryColor));
+		up[3].setBackground(getColor(cube.w[3].primaryColor));
+		left[1].setBackground(getColor(cube.w[3].secondaryColor));
+		//middle layer wedges
+		left[5].setBackground(getColor(cube.w[8].secondaryColor));
+		front[3].setBackground(getColor(cube.w[8].primaryColor));
+		front[5].setBackground(getColor(cube.w[9].primaryColor));
+		right[3].setBackground(getColor(cube.w[9].secondaryColor));
+		right[5].setBackground(getColor(cube.w[10].secondaryColor));
+		back[5].setBackground(getColor(cube.w[10].primaryColor));
+		back[3].setBackground(getColor(cube.w[11].primaryColor));
+		left[3].setBackground(getColor(cube.w[11].secondaryColor));
+		//BottomLayer wedges
+		bottom[1].setBackground(getColor(cube.w[4].primaryColor));
+		front[7].setBackground(getColor(cube.w[4].secondaryColor));
+		bottom[5].setBackground(getColor(cube.w[5].primaryColor));
+		right[7].setBackground(getColor(cube.w[5].secondaryColor));
+		bottom[7].setBackground(getColor(cube.w[6].primaryColor));
+		back[1].setBackground(getColor(cube.w[6].secondaryColor));
+		bottom[3].setBackground(getColor(cube.w[7].primaryColor));
+		left[7].setBackground(getColor(cube.w[7].secondaryColor));
+		//Top Layer Corners
+		up[6].setBackground(getColor(cube.c[0].ubColor));
+		front[0].setBackground(getColor(cube.c[0].fbColor));
+		left[2].setBackground(getColor(cube.c[0].rlColor)); 
+		
+		up[8].setBackground(getColor(cube.c[1].ubColor));
+		front[2].setBackground(getColor(cube.c[1].fbColor));
+		right[0].setBackground(getColor(cube.c[1].rlColor));
+		
+		up[2].setBackground(getColor(cube.c[2].ubColor));
+		back[8].setBackground(getColor(cube.c[2].fbColor));
+		right[2].setBackground(getColor(cube.c[2].rlColor));
+		
+		up[0].setBackground(getColor(cube.c[3].ubColor));
+		back[6].setBackground(getColor(cube.c[3].fbColor));
+		left[0].setBackground(getColor(cube.c[3].rlColor));
+		//Bottom Layer Corners
+		bottom[0].setBackground(getColor(cube.c[4].ubColor));
+		front[6].setBackground(getColor(cube.c[4].fbColor));
+		left[8].setBackground(getColor(cube.c[4].rlColor)); 
+		
+		bottom[2].setBackground(getColor(cube.c[5].ubColor));
+		front[8].setBackground(getColor(cube.c[5].fbColor));
+		right[6].setBackground(getColor(cube.c[5].rlColor));
+		
+		bottom[8].setBackground(getColor(cube.c[6].ubColor));
+		back[2].setBackground(getColor(cube.c[6].fbColor));
+		right[8].setBackground(getColor(cube.c[6].rlColor));
+		
+		bottom[6].setBackground(getColor(cube.c[7].ubColor));
+		back[0].setBackground(getColor(cube.c[7].fbColor));
+		left[6].setBackground(getColor(cube.c[7].rlColor));
+		
+	}
+
+
+	public Color getColor(int c) {
+		switch(c) {
+		case 0: return Color.BLUE;
+		case 1: return Color.GREEN;
+		case 2: return Color.WHITE;
+		case 3: return Color.YELLOW;
+		case 4: return Color.RED;
+		default: return Color.ORANGE;
 		}
 	}
 }
